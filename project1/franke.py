285553852805ba0192d1a4010512da39e9118a08
Bruce Chappell <brucechappelliii@gmail.com>
2019-09-06 22:09:17 +0200

    not sure its late

--- project1/franke.py
+++ project1/franke.py
@@ -15,28 +15,27 @@ def FrankeFunction(x,y):
     return term1 + term2 + term3 + term4
 
 
-if __name__ == "__main__":
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-
-    # Make data.
-    x = np.arange(0, 1, 0.05)
-    y = np.arange(0, 1, 0.05)
-    x, y = np.meshgrid(x,y)
-
-
-    z = FrankeFunction(x, y)
-
-    # Plot the surface.
-    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
-
-    # Customize the z axis.
-    ax.set_zlim(-0.10, 1.40)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
-    plt.show()
+# if __name__ == "__main__":
+#     fig = plt.figure()
+#     ax = fig.gca(projection='3d')
+#
+#     # Make data.
+#     x = np.arange(0, 1, 0.05)
+#     y = np.arange(0, 1, 0.05)
+#     x, y = np.meshgrid(x,y)
+#
+#
+#     z = FrankeFunction(x, y)
+#
+#     # Plot the surface.
+#     surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+#
+#     # Customize the z axis.
+#     ax.set_zlim(-0.10, 1.40)
+#     ax.zaxis.set_major_locator(LinearLocator(10))
+#     ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+#
+#     # Add a color bar which maps values to colors.
+#     fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+#     plt.show()
